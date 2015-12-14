60d2d58a3b599eba64db61a322aa65de66ef447b
Markus Kont <markuskont@gmail.com>
2015-12-14 22:23:45 +0200

    Familiarized myself with require.js

---
+++ js/connection.js
@@ -0,0 +1,10 @@
+define(['elasticsearch'], function (elasticsearch) {
+    "use strict";
+    var client = new elasticsearch.Client({
+    	host: {
+    		protocol: 'http',
+    		host: '192.168.56.11',
+    		port: '9200'
+		}
+    });
+});
